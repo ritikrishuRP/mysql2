4ab0a02278b5100a14e9c3f240f3bb0389055f3f
rishu <rrishu212@gmail.com>
2024-09-15 13:58:57 -0700

    sequel association

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database.js');
+const Product = require('./models/product');
+const User = require('./models/user')
 
 
 var cors = require('cors');
@@ -29,6 +31,15 @@ const expenseRoutes = require('./routes/expense')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => consolr.log(err));
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use('/user', userRoutes);
@@ -39,9 +50,24 @@ app.use('/expense', expenseRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, {constratints: true, onDelete: 'CASCADE'});
+//HERE WE CAN DEFINE HOW RELATIONSHIP MANAGED, if user deleted all it's product deleted refers by cascade
+User.hasMany(Product);
+
 sequelize.sync().then(result => {
+     return User.findByPk(1);
     //console.log(result);
+})
+.then(user => {
+    if(!user) {
+        return User.create({name: 'ritik', email: 'rrishu212@gmail.com'})
+    }
+    return user;
+})
+.then(user => {
+    console.log(user);
     app.listen(3000);
-}).catch(err => console.log(err))
+})
+.catch(err => console.log(err))
 
 
